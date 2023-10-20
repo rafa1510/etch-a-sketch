f12187327ad8a5031bdef8e318fe7975360cd755
rafa1510 <rcestti78@gmail.com>
2023-10-19 22:27:07 -0400

    Prompt user for desired squares per row & column
    On click of the button in the homepage, it will prompt the user for
    their  desired square number for row and column, and makes sure that it
    is greater than 0, less than 100, and a number

--- script.js
+++ script.js
@@ -18,3 +18,29 @@
         if mouse passes through dynamicDiv, add hovered class
 
 */
+
+const button = document.querySelector("#button");
+
+let promptValue = 16; // Default amount of squares is 16
+
+button.addEventListener("click", () => {
+    // Prompts the user for a number and turns it into an integer
+    promptValue = parseInt(prompt("How many squares per row and column would you like?"));
+
+    while (promptValue > 100 || promptValue < 0 || isNaN(promptValue))
+    {
+        if (promptValue > 100)
+        {
+            promptValue = parseInt(prompt("Please enter a number less than 100"));
+        }
+        if (promptValue < 0)
+        {
+            promptValue = parseInt(prompt("Please enter a positive number"));
+        }
+        // Checks if promptValue is NaN, as parseInt() will return NaN if the user doesn't input a number
+        if (isNaN(promptValue))
+        {
+            promptValue = parseInt(prompt("Please enter a number"));
+        }
+    }
+})
